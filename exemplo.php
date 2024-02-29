7b2042f3e3e133966495fcbd2a06070a5a7f8321
Gonçalo Valentim <goncalo.valentim.2022167@my.istec.pt>
2024-02-29 16:22:40 +0000

    exemplo ficheiro php

---
+++ exemplo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Número Aleatório</title>
+</head>
+<body>
+    <h1>Número Aleatório</h1>
+    <?php
+    // Gerar um número aleatório entre 1 e 100
+    $numero_aleatorio = rand(1, 100);
+    // Exibir o número aleatório
+    echo "<p>O número aleatório gerado é: $numero_aleatorio</p>";
+    ?>
+</body>
+</html>
